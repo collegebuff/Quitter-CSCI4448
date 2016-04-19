738b13fc05c58ab528da032724b38a7520a02a48
Bryce <bryce.strick@gmail.com>
2016-04-19 11:59:22 -0600

    Testing file

---
+++ sentiment_analysis/tester.py
@@ -0,0 +1,4 @@
+import interface
+
+print interface.predictTweet("I hate you")
+print interface.predictList(["I hate you", "I feel ambivalent about you.", "Trump in General", "Violence", "I love everything"])
